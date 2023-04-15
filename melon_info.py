02a14b058ef9684651405bbcd895ce8326a755b4
ElAbdelali <eljaouhariabdelali@outlook.com>
2023-04-15 17:08:52 -0500

    Created a dictionary which contains the prices, name, and whether or not it is seedless. Formatted the melon_info.py to output the information in a specific format

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,18 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_prices, melon_seedlessness 
+from melons import melons 
 
 
-def print_melon(name, price, seedless):
+def print_melon():
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+    for melon in melons:
+        print(f"""
+            melon: {melon}
+            price: {melons[melon][0]}
+            seedless: {melons[melon][1]}
+            flesh_color: {melons.get('flesh_color', None)}
+            rind_color: {melons.get('grind_color', None)}
+            average_weight: {melons.get('average_weight', None)}""")
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price}')
-
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i])
--- melons.py
+++ melons.py
@@ -1,23 +1,6 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
+melons = {'Honeydew': (.99, True), 
+          'Crenshaw': (2.00, False),
+          'Crane': (2.50, False),
+          'Casaba': (2.50, False),
+          'Cantaloupe': (.99, False)}
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
